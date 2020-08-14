9fb4227d220e74ef100886461abce3ce833a32af
Stellar Creator <stellar@creator>
2020-08-14 10:52:48 +0500

    Add queue support

---
+++ uQueue.c
@@ -0,0 +1,40 @@
+/*
+Autor:          Pavel
+Date Of Update: 14.08.2020
+File:           uQueue.c
+*/
+
+#include "uGlobal.h"
+#include "uQueue.h"
+
+#ifndef uMaxQueueSize
+#error Please set macros 'uMaxQueueSize', now macros isn't set. This value is maximum size of queue.
+#endif
+
+uFunction uQueueAdd(Queue *q, uInteger data){
+    if(q->index > uMaxQueueSize){
+    	q->index = 0;
+    }
+	q->data[q->index] = data;
+    q->index++;
+}
+
+uInteger uQueueGet(Queue *q){
+    uInteger backfeed = 0;
+    if(q->index > 0){
+        backfeed = q->data[q->index - 1];
+        q->data[q->index - 1] = 0;
+        q->index--;
+    }else{
+        backfeed = 0;
+    }
+    return backfeed;
+}
+
+uInteger uQueueIsReady(Queue *q){
+    if(q->index > 0){
+    	return uState_Ready;
+    }else{
+    	return uState_Done;
+    }
+}
---
+++ uQueue.h
@@ -0,0 +1,24 @@
+/*
+Autor:			Pavel
+Date Of Update: 05.08.2020
+File:			uQueue.h
+*/
+
+#ifndef UQUEUE_H
+#define UQUEUE_H
+
+#include "uGlobal.h"
+
+// Structs
+typedef struct  {
+    uInteger data[64];
+    uInteger index;
+    uState isReady;
+} uQueue;
+
+// Functions
+uFunction	uQueueAdd(uQueue *q, uInteger data);
+uInteger	uQueueGet(uQueue *q);
+uInteger	uQueueIsReady(uQueue *q);
+
+#endif /* UQUEUE_H */
